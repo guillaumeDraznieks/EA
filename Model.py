041e3ee3bacfc8b63468922a612b86f437a066b9
guillaumeDraznieks <guillaume.draznieks@polytechnique.edu>
2021-10-07 19:09:51 +0200

    First real commit

--- Model.py
+++ Model.py
@@ -1,5 +1,39 @@
-import matplotlib.pyplot as plt
-import numpy as np
+# Systèmes 1D
+# Pas de discrétisation
+# Conditions aux limites périodiques
 
-def yo():
-    return 1
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Model1D():
+  def __init__(self, longueur, n_mailles, dimension, myPDESystem):
+    self.longueur = longueur
+    self.n_mailles = n_mailles
+    self.n=dimension
+    self.f=myPDESystem.f
+    self.J=myPDESystem.J
+    self.x = self.longueur*np.array(range(self.n_mailles+1))/self.n_mailles
+    self.dx = self.longueur/self.n_mailles
+    self.x_ctr = self.x[:-1]+self.dx/2
+    self.U=np.zeros((self.n_mailles,self.n))
+    self.T=0
+
+  def maxSoundSpeed(self):
+    maxi = -1
+    for i in range(self.n_mailles):
+      maxi = max(maxi, np.max(np.abs(np.linalg.eigvals(self.J(self.U[i])))))
+    self.max_speed = maxi
+    return maxi
+  
+  def plot(self):
+    if self.n>1:
+      y = np.sin(self.x_ctr ** 2)    
+      fig, axs = plt.subplots(self.n)
+      fig.suptitle('Etat du système au temps '+str(self.T))
+      for i in range(self.n):
+        axs[i].plot(self.x_ctr, self.U[:,i])
+    else:
+      y = np.sin(self.x_ctr ** 2)    
+      fig, axs = plt.subplots()
+      fig.suptitle('Etat du système au temps '+str(self.T))
+      axs.plot(self.x_ctr, self.U[:,0])
